4bab8a4b8b44850527e2b1a19626d89cf33c2b56
Trupesh Patel <trpatel2@crimson.ua.edu>
2023-11-27 17:19:35 -0600

    fix: segmentation fault added ulimits and wheels for t-route clean up in external library installs added sqlight and enabled it in NGen

--- singularity/templates/netcdf/install_netcdf_cxx.sh
+++ singularity/templates/netcdf/install_netcdf_cxx.sh
@@ -18,7 +18,7 @@ cd /tmp/netcdf
 curl -s https://api.github.com/repos/Unidata/netcdf-cxx4/releases/latest | grep "tarball_url" | cut -d '"' -f 4 | xargs curl -L -o netcdf-cxx4-latest.tar.gz ;
 mkdir netcdf-cxx4-latest
 tar -xvzf netcdf-cxx4-latest.tar.gz -C netcdf-cxx4-latest --strip 1
-mkdir netcdf-cxx4-latest/build && cd netcdf-cxx4-latest/build
+mkdir -p netcdf-cxx4-latest/build && cd netcdf-cxx4-latest/build
 ../configure --enable-parallel --prefix=/usr
 cmake ..
 make -j$(nproc) install
--- singularity/templates/t-route/install_t_route.sh
+++ singularity/templates/t-route/install_t_route.sh
@@ -16,9 +16,22 @@ echo ""
 echo "==========================================================="
 echo "-- Now Cloning and Installing NOAA-OWP T-Route ..."
 echo "-----------------------------------------------------------"
-# export FC=gfortran NETCDF=/usr/include
-git clone --progress --single-branch --branch master http://github.com/NOAA-OWP/t-route.git
+export FC=gfortran NETCDF=/usr/include
+ulimit -n 10000
+git clone --progress --single-branch --branch master https://github.com/NOAA-OWP/t-route
 cd t-route
-cp /tmp/t-route/compiler.sh .
-
 ./compiler.sh no-e
+
+cd /t-route/src/troute-network
+python setup.py --use-cython bdist_wheel
+
+cd /t-route/src/troute-routing
+python setup.py --use-cython bdist_wheel
+
+cd /t-route/src/troute-nwm
+python setup.py bdist_wheel
+
+cd /t-route
+cp /t-route/src/troute-network/dist/*.whl /t-route/wheels
+cp /t-route/src/troute-routing/dist/*.whl /t-route/wheels
+cp /t-route/src/troute-nwm/dist/*.whl /t-route/wheels
